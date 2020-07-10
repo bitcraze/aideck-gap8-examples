43837aa052d6cb44ee76a472b1efcfbd00d8951a
knmcguire <kimberly@bitcraze.io>
2020-07-10 13:52:53 +0200

    (#15) add a demosaicking function to test camera

--- GAP8/test_functionalities/test_camera/test.c
+++ GAP8/test_functionalities/test_camera/test.c
@@ -18,13 +18,16 @@
 #include "bsp/camera.h"
 #include "bsp/camera/himax.h"
 
-#include "ImgIO.h"
+#include "gaplib/ImgIO.h"
 
 #define WIDTH    324
 #define HEIGHT   244
 #define BUFF_SIZE (WIDTH*HEIGHT)
 
 PI_L2 unsigned char *buff;
+
+PI_L2 unsigned char *buff_output;
+
 static struct pi_device camera;
 static volatile int done;
 
@@ -49,6 +52,77 @@ static int open_camera(struct pi_device *device)
     return 0;
 }
 
+static void demosaicking(char *input, char* output, int width, int height)
+{
+    int idx = 0;
+    int idxr[8];
+    char red, blue, green;
+
+    for (int y = 0; y < height ; y++)
+    {
+        for (int x = 0; x < width ; x++)
+        {
+            int idx = y * width + x;
+
+            if (x == 0 || y == 0 || x == width-1 || y == height-1)
+            {
+                output[idx * 3] = 0;
+                output[idx * 3 + 1] = 0;
+                output[idx * 3 + 2] = 0;
+            }
+            else
+            {
+
+                idxr[0] = (y - 1) * width + (x - 1);
+                idxr[1] = (y)*width + (x - 1);
+                idxr[2] = (y + 1) * width + (x - 1);
+                idxr[3] = (y + 1) * width + (x);
+                idxr[4] = (y + 1) * width + (x + 1);
+                idxr[5] = (y)*width + (x + 1);
+                idxr[6] = (y - 1) * width + (x + 1);
+                idxr[7] = (y - 1) * width + (x);
+
+                int x_shift = 0;
+                int y_shift = 0;
+
+                if ((x + x_shift) % 2 == 0 && (y + y_shift) % 2 == 0) //R
+                {
+                    red = input[idx];
+                    blue = (input[idxr[0]] + input[idxr[2]] + input[idxr[4]] + input[idxr[6]]) / 4;
+                    green = (input[idxr[1]] + input[idxr[3]] + input[idxr[5]] + input[idxr[7]]) / 4;
+                }
+                else if ((x + x_shift) % 2 == 1 && (y + y_shift) % 2 == 0) //G2
+                {
+                    red = (input[idxr[1]] + input[idxr[5]]) / 2;
+                    blue = (input[idxr[3]] + input[idxr[7]]) / 2;
+                    green = input[idx];
+                }
+                else if ((x + x_shift) % 2 == 0 && (y + y_shift) % 2 == 1) //G1
+                {
+                    red = (input[idxr[3]] + input[idxr[7]]) / 2;
+                    blue = (input[idxr[1]] + input[idxr[5]]) / 2;
+                    green = input[idx];
+                }
+                else if ((x + x_shift) % 2 == 1 && (y + y_shift) % 2 == 1) //B
+                {
+                    red = (input[idxr[0]] + input[idxr[2]] + input[idxr[4]] + input[idxr[6]]) / 4;
+                    blue = input[idx];
+                    green = (input[idxr[1]] + input[idxr[3]] + input[idxr[5]] + input[idxr[7]]) / 4;
+                }
+                else
+                {
+                    red = 0;
+                    green = 0;
+                    blue = 0;
+                }
+
+                output[idx * 3] = red;
+                output[idx * 3 + 1] = green;
+                output[idx * 3 + 2] = blue;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -68,6 +142,7 @@ int main()
         return -1;
     }
 
+
     // Rotate camera orientation
     uint8_t set_value=3;
     uint8_t reg_value;
@@ -79,6 +154,14 @@ int main()
     // Reserve buffer space for image
     buff = pmsis_l2_malloc(WIDTH*HEIGHT);
     if (buff == NULL){ return -1;}
+
+    #ifdef COLOR_IMAGE
+    buff_output = pmsis_l2_malloc(WIDTH*HEIGHT*3);
+    if (buff_output == NULL){ return -1;}
+    #endif
+    printf("Initialized buffers\n");
+
+
 
     #ifdef ASYNC_CAPTURE
     // Start up async capture task
@@ -93,12 +176,18 @@ int main()
     #else
     pi_camera_capture(&camera, buff, BUFF_SIZE);
     #endif
-    
+
+    #ifdef COLOR_IMAGE
+    demosaicking(buff, buff_output,WIDTH,HEIGHT);
+    #endif
+
     // Stop the camera and immediately close it
     pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
     pi_camera_close(&camera);
     
     // Write to file
-    WriteImageToFile("../../../img_out.ppm", WIDTH, HEIGHT, buff);
-
+    #ifdef COLOR_IMAGE
+    WriteImageToFile("../../../img_color.ppm", WIDTH, HEIGHT,sizeof(uint32_t), buff_output, RGB888_IO);
+    #endif
+    WriteImageToFile("../../../img_raw.ppm", WIDTH, HEIGHT,sizeof(uint8_t), buff, GRAY_SCALE_IO );
 }
