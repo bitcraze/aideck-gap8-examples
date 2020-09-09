2f989439b6f25d1203e39274fef485bdef960b71
Nick Heppert <nick.heppert@gmx.de>
2020-09-09 11:57:50 +0200

    Correct demosaicking grayscale offset

--- GAP8/common/img_proc.h
+++ GAP8/common/img_proc.h
@@ -20,9 +20,13 @@ inline void demosaicking(char *input, char* output, int width, int height, int g
 
             if (x == 0 || y == 0 || x == width-1 || y == height-1)
             {
-                output[idx * 3] = 0;
-                if(!grayscale)
+                if(grayscale)
                 {
+                    output[idx] = 0;
+                }
+                else
+                {
+                    output[idx * 3] = 0;
                     output[idx * 3 + 1] = 0;
                     output[idx * 3 + 2] = 0;
                 }
