3f481fe1ecbd0fe9a385a0118ac7c2bc2a92c233
gemenerik <rikjbouwmeester@gmail.com>
2022-03-18 12:10:56 +0100

    Save streamed images to stream_out folder (both raw and debayered/color)

--- GAP8/test_functionalities/wifi_jpeg_streamer/opencv-viewer.py
+++ GAP8/test_functionalities/wifi_jpeg_streamer/opencv-viewer.py
@@ -112,8 +112,11 @@ while(1):
           bayer_img = np.frombuffer(imgStream, dtype=np.uint8)   
           #img2 = np.fromfile("img.raw", dtype=np.uint8)
           bayer_img.shape = (244, 324)
+          color_img = cv2.cvtColor(bayer_img, cv2.COLOR_BayerBG2BGRA)
           cv2.imshow('Bayer', bayer_img)
-          cv2.imshow('Color', cv2.cvtColor(bayer_img, cv2.COLOR_BayerBG2BGRA))
+          cv2.imshow('Color', color_img)
+          cv2.imwrite(f"stream_out/raw/img_{count:06d}.png", bayer_img)
+          cv2.imwrite(f"stream_out/debayer/img_{count:06d}.png", color_img)
           cv2.waitKey(1)
       else:
           with open("img.jpeg", "wb") as f:
